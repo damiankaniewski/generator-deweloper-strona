c916e26cf3d70a8c20e95da9b8d47ddfcfc2982e
Damian <daba9@op.pl>
2025-01-10 22:11:31 +0100

    photos added

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,9 +7,27 @@ import { Component } from '@angular/core';
 })
 export class GalleryComponent {
   images: string[] = [
-    'assets/images/MietniowTitle.jpg',
-    'assets/images/NiepolomiceTitle.jpg',
-    'assets/images/LednicaGornaTitle.jpg',
+    'assets/images/LednicaGorna/Lednica_1.jpg',
+    'assets/images/LednicaGorna/Lednica_2.jpg',
+    'assets/images/LednicaGorna/Lednica_3.jpg',
+    'assets/images/LednicaGorna/Lednica_4.jpg',
+    'assets/images/LednicaGorna/Lednica_5.jpg',
+    'assets/images/LednicaGorna/Lednica_6.jpg',
+    'assets/images/LednicaGorna/Lednica_7.jpg',
+    'assets/images/LednicaGorna/Lednica_8.jpg',
+    'assets/images/LednicaGorna/Lednica_9.jpg',
+    'assets/images/LednicaGorna/Lednica_10.jpg',
+    'assets/images/Mietniow/Mietniow_1.jpg',
+    'assets/images/Mietniow/Mietniow_2.jpg',
+    'assets/images/Mietniow/Mietniow_3.jpg',
+    'assets/images/Mietniow/Mietniow_4.jpg',
+    'assets/images/Mietniow/Mietniow_5.jpg',
+    'assets/images/Mietniow/Mietniow_6.jpg',
+    'assets/images/Mietniow/Mietniow_7.jpg',
+    'assets/images/Mietniow/Mietniow_8.jpg',
+    'assets/images/Mietniow/Mietniow_9.jpg',
+    'assets/images/Mietniow/Mietniow_10.jpg',
+    'assets/images/Mietniow/Mietniow_11.jpg',
   ];
   currentIndex: number = 0;
 
